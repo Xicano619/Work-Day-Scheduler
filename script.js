cd549409cf95d897f3fb4a1366a6dc112bcff273
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-10-29 17:49:13 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // Create variable for moment live date.
+    var moment = moment();
+      
+      
+    $("#currentDay").text(moment.format('LLL'));
+      
+    });
+    console.log(moment);
+     
+       
