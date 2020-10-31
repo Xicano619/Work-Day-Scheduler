f6a39856b7375d6daaa3351caa6d165b88b3ac0b
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-10-31 08:55:57 -0700

    Set inetrval & got saveBtn to work

--- script.js
+++ script.js
@@ -5,8 +5,35 @@ $(document).ready(function(){
     // Display Live-date.
     $("#currentDay").text(m.format('LLL'));
     
+    //Coordinate the classes .past .present .future time block with live date.
+    setInterval(function(){ 
+    $(".time-block").each(function(){
+       var time = moment().hour();
+        var data = $(this).data("time");
+        if (data < time){
+            $(this).addClass("past");
+        }else if (data === time){
+            $(this).addClass("present");
+            $(this).removeClass("past");
+        }else{
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+        
+     });},2000);
+    
+    
+    // Get saveBtn to work.
+    $(".saveBtn").on("click",function(){
+        var saveNote = $(this).parent().data("time");
+        var textarea = $(this).siblings("textarea").val();
+
+        localStorage.setItem(saveNote, textarea);
     });
-//    Coordinate the .past .present .future time block with live date.
-    // var colorTimeBlock = $("row time-block").css(past.present.future).text(m);
+    // get local stored data and populace textarea.
+    // $(".time-block[data-time='9'] textarea").val(localStorage.getItem("9"));
+    
+});
      
        
